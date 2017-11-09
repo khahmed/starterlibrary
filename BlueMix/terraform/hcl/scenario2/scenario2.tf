9bf1a4a1ffbcbb9ef73394b394aa35d08416a666
Khalid Ahmed <kahmed0610@gmail.com>
2017-11-09 09:37:54 -0500

    Update scenario2.tf

--- BlueMix/terraform/hcl/scenario2/scenario2.tf
+++ BlueMix/terraform/hcl/scenario2/scenario2.tf
@@ -9,18 +9,11 @@ variable "datacenter" {
   description = "Softlayer datacenter where infrastructure resources will be deployed"
 }
 
-variable "first_hostname" {
-  description = "Hostname of the first virtual instance (small flavor) to be deployed"
-  default = "debian-small"
-}
 
-variable "second_hostname" {
+
+variable "first_hostname" {
   description = "Hostname of the second virtual instance (medium flavor) to be deployed"
   default = "ubuntu-medium"
-}
-
-data "ibm_compute_image_template" "debian_8_6_64" {
-    name = "100GB - Debian / Debian / 8.0.0-64 Minimal for VSI"
 }
 
 # This will create a new SSH key that will show up under the \
@@ -30,26 +23,11 @@ resource "ibm_compute_ssh_key" "orpheus_public_key" {
     public_key = "${var.public_ssh_key}"
 }
 
-# Create a new virtual guest using image "Debian"
-resource "ibm_compute_vm_instance" "debian_small_virtual_guest" {
-    hostname = "${var.first_hostname}"
-    image_id = "${data.ibm_compute_image_template.debian_8_6_64.id}"
-    domain = "cam.ibm.com"
-    datacenter = "${var.datacenter}"
-    network_speed = 10
-    hourly_billing = true
-    private_network_only = false
-    cores = 1
-    memory = 1024
-    user_metadata = "{\"value\":\"newvalue\"}"
-    dedicated_acct_host_only = false
-    local_disk = false
-    ssh_key_ids = ["${ibm_compute_ssh_key.orpheus_public_key.id}"]
-}
+
 
 # Create a new virtual guest using image "Ubuntu"
 resource "ibm_compute_vm_instance" "ubuntu_medium_virtual_guest" {
-    hostname = "${var.second_hostname}"
+    hostname = "${var.first_hostname}"
     #image_id = "${data.ibm_compute_image_template.ubuntu_16_04_01_64.id}"
     os_reference_code="UBUNTU_16_64"
     domain = "cam.ibm.com"
@@ -58,7 +36,7 @@ resource "ibm_compute_vm_instance" "ubuntu_medium_virtual_guest" {
     hourly_billing = true
     private_network_only = false
     cores = 2
-    memory = 4096
+    memory = 8096
     user_metadata = "{\"value\":\"newvalue\"}"
     dedicated_acct_host_only = false
     local_disk = false
